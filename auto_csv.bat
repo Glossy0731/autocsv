0752e05f01974035fa0a0fdf07f09709684ce5f9
Glossy0731 <129810612+Glossy0731@users.noreply.github.com>
2023-04-06 17:31:55 +0900

    Add files via upload

--- auto_csv.bat
+++ auto_csv.bat
@@ -1,2 +1,8 @@
 @echo off
-powershell.exe -ExecutionPolicy Bypass -File "run.ps1" >> "log.txt" 2>&1
+setlocal
+
+:: 获取脚本文件的路径
+set "SCRIPT_PATH=%~dp0"
+
+:: 执行 PowerShell 脚本并将输出重定向到日志文件中
+powershell.exe -ExecutionPolicy Bypass -File "%SCRIPT_PATH%run.ps1" >> "%SCRIPT_PATH%log.txt" 2>&1
