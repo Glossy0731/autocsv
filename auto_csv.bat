39a426c316c7975fa517b5c39fa3eb91b8fba2ff
Glossy0731 <129810612+Glossy0731@users.noreply.github.com>
2023-04-06 18:50:35 +0900

    Add files via upload

--- auto_csv.bat
+++ auto_csv.bat
@@ -1,2 +1,16 @@
 @echo off
-powershell.exe -ExecutionPolicy Bypass -Command "& { Get-Date -Format 'yyyy-MM-dd HH:mm:ss'; & .\auto_csv.ps1 }" >> "log.txt"
+
+REM 定义要转换的CSV文件夹路径
+set folder="C:\CSV Files"
+
+REM 定义PowerShell脚本内容
+set script=^
+$config = Get-Content .\config.txt ;^
+$folder = $config.Trim() ;^
+function ConvertTo-Excel { ... } ;^
+while ($true) { ... }
+
+REM 调用PowerShell执行脚本，并将输出记录到log.txt文件中
+powershell.exe -ExecutionPolicy Bypass -Command "%script%" -Encoding utf8 >> "log.txt"
+
+pause
