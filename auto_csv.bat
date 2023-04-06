ca8b9980f03d1904d2b8e82d81e8cf6d0fb9f9f9
Glossy0731 <129810612+Glossy0731@users.noreply.github.com>
2023-04-06 17:38:03 +0900

    Add files via upload

--- auto_csv.bat
+++ auto_csv.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 
-:: 获取脚本文件的路径
-set "SCRIPT_PATH=%~dp0"
+:: PowerShellスクリプトファイルのパスを取得する
+set "SCRIPT_PATH=C:\Users\gloss\Downloads"
 
-:: 执行 PowerShell 脚本并将输出重定向到日志文件中
-powershell.exe -ExecutionPolicy Bypass -File "%SCRIPT_PATH%run.ps1" >> "%SCRIPT_PATH%log.txt" 2>&1
+:: PowerShellスクリプトを実行し、出力をログファイルにリダイレクトする
+powershell.exe -ExecutionPolicy Bypass -File "%SCRIPT_PATH%auto_csv.ps1" >> "%SCRIPT_PATH%log.txt" 2>&1
